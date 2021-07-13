82adaaeb74a18af12a22b9bdd31df333417cf9a4
MaxIbanez <m.ibaezmolina@uandresbello.edu>
2021-07-13 14:09:13 -0400

    Regiones de: Arica y Parinacota, Tarapacá y Antofagasta. Han sido agregadas.

--- regiones-provincias-comunas.js
+++ regiones-provincias-comunas.js
@@ -1,18 +1,79 @@
+/*
+    Creado por Maximiliano Ibáñez M.
+*/
+
 regiones = [
-    {id:1,nombre:"Región de Arica y Parinacota"},
-    {id:2,nombre:"Región de Tarapacá"},
-    {id:3,nombre:"Región de Antofagasta"},
-    {id:4,nombre:"Región de Atacama"},
-    {id:5,nombre:"Región de Coquimbo"},
-    {id:6,nombre:"Región de Valparaíso"},
-    {id:7,nombre:"Región del Libertador General Bernardo O'Higgins"},
-    {id:8,nombre:"Región del Maule"},
-    {id:9,nombre:"Región de Ñuble"},
-    {id:10,nombre:"Región del Biobío"},
-    {id:11,nombre:"Región de La Araucanía"},
-    {id:12,nombre:"Región de Los Ríos"},
-    {id:13,nombre:"Región de Los Lagos"},
-    {id:14,nombre:"Región de Aysén del G. Carlos Ibáñez del Campo"},
-    {id:15,nombre:"Región de Magallanes y de la Antártica Chilena"},
-    {id:16,nombre:"Región Metropolitana de Santiago"}
+    {id_region:1,nombre:"Región de Arica y Parinacota"},
+    {id_region:2,nombre:"Región de Tarapacá"},
+    {id_region:3,nombre:"Región de Antofagasta"},
+    {id_region:4,nombre:"Región de Atacama"},
+    {id_region:5,nombre:"Región de Coquimbo"},
+    {id_region:6,nombre:"Región de Valparaíso"},
+    {id_region:7,nombre:"Región del Libertador General Bernardo O'Higgins"},
+    {id_region:8,nombre:"Región del Maule"},
+    {id_region:9,nombre:"Región de Ñuble"},
+    {id_region:10,nombre:"Región del Biobío"},
+    {id_region:11,nombre:"Región de La Araucanía"},
+    {id_region:12,nombre:"Región de Los Ríos"},
+    {id_region:13,nombre:"Región de Los Lagos"},
+    {id_region:14,nombre:"Región de Aysén del G. Carlos Ibáñez del Campo"},
+    {id_region:15,nombre:"Región de Magallanes y de la Antártica Chilena"},
+    {id_region:16,nombre:"Región Metropolitana de Santiago"}
+]
+
+provincias = [
+    {id_prov:20,nombre:"Arica",id_region:1},
+    {id_prov:21,nombre:"Parinacota",id_region:1},
+    {id_prov:22,nombre:"Iquique",id_region:2},
+    {id_prov:23,nombre:"Tamarugal",id_region:2},
+    {id_prov:24,nombre:"Antofagasta",id_region:3},
+    {id_prov:25,nombre:"Tocopilla",id_region:3},
+    {id_prov:26,nombre:"El Loa",id_region:3},
+    {id_prov:27,nombre:"Copiapó",id_region:4},
+    {id_prov:28,nombre:"Huasco",id_region:4},
+    {id_prov:29,nombre:"Chañaral",id_region:4},
+
+
+
+]
+comunas = [
+    {id_comuna:101, nombre:"Arica", id_prov:20},
+    {id_comuna:102, nombre:"Camarones", id_prov:20},
+    //_____________________________________________
+    {id_comuna:103, nombre:"Putre", id_prov:21},
+    {id_comuna:104, nombre:"General Lagos", id_prov:21},
+    //_____________________________________________
+    {id_comuna:105, nombre:"Iquique", id_prov:22},
+    {id_comuna:106, nombre:"Alto Hospicio", id_prov:22},
+    //_____________________________________________
+    {id_comuna:107, nombre:"Pozo Almonte", id_prov:23},
+    {id_comuna:108, nombre:"Camiña", id_prov:23},
+    {id_comuna:109, nombre:"Colchane", id_prov:23},
+    {id_comuna:110, nombre:"Huara", id_prov:23},
+    {id_comuna:111, nombre:"Pica", id_prov:23},
+    //_____________________________________________
+    {id_comuna:112, nombre:"Antofagasta", id_prov:24},
+    {id_comuna:113, nombre:"Mejillones", id_prov:24},
+    {id_comuna:114, nombre:"Sierra Gorda", id_prov:24},
+    {id_comuna:115, nombre:"Taltal", id_prov:24},
+    //_____________________________________________
+    {id_comuna:116, nombre:"Tocopilla", id_prov:25},
+    {id_comuna:117, nombre:"María Elena", id_prov:25},
+    //_____________________________________________
+    {id_comuna:118, nombre:"Calama", id_prov:26},
+    {id_comuna:119, nombre:"Ollagüe", id_prov:26},
+    {id_comuna:120, nombre:"San Pedro de Atacama", id_prov:26},
+    //______________________________________________
+    {id_comuna:121, nombre:"Copiapó", id_prov:27},
+    {id_comuna:122, nombre:"Caldera", id_prov:27},
+    {id_comuna:123, nombre:"Tierra Amarilla", id_prov:27},
+    //______________________________________________
+    {id_comuna:124, nombre:"Vallenar", id_prov:28},
+    {id_comuna:125, nombre:"Alto del Carmen", id_prov:28},
+    {id_comuna:126, nombre:"Freirina", id_prov:28},
+    {id_comuna:127, nombre:"Huasco", id_prov:28},
+    //______________________________________________
+    {id_comuna:128, nombre:"Chañaral", id_prov:29},
+    {id_comuna:129, nombre:"Diego de Almagro", id_prov:29}
+
 ]
